1647a30368abbd8b9a4839173552966ecdc03c0d
Richard McCarthy <mccarthy.richard@gmail.com>
2015-01-23 10:06:29 +0000

    Adding new SoftwareDesk Bundle project configuration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new SoftwareDesk\BikeTraderAPIBundle\SoftwareDeskBikeTraderAPIBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
