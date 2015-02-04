e028ccf92b1b8f2f7aea1ea35b47c6c289daeb9c
Richard McCarthy <mccarthy.richard@gmail.com>
2015-02-04 16:56:52 +0000

    updated the AppKernel with the removal of the original BikeTraderAPIBundle entry

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,6 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
-            new SoftwareDesk\BikeTraderAPIBundle\SoftwareDeskBikeTraderAPIBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
         );
 
